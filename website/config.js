fcc1f81b6bb08f1779fefb4e0b87cd78f37dbf0f
synthaze <florian.malard@gmail.com>
2021-12-24 17:20:23 +0100

    None

---
+++ website/config.js
@@ -0,0 +1,21 @@
+var parentCoin = "babycoin";
+
+var api = "/api";
+var poolHost = "babycoin.multi-pool.net";
+
+var email = "support@poolhost.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = "en";
+
+// Merged Mining:
+// var api = "http://poolhost/apiMerged";
+// var blockchainExplorer = "http://explorer.ird.cash/?hash={id}#block";
+// var transactionExplorer = "http://explorer.ird.cash/?hash={id}#transaction";
